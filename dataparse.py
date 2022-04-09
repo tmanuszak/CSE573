614a5f425db9b53fb74362f4de26dc2b36f52f20
Trey Manuszak <manuszaktrey@gmail.com>
2022-04-09 16:21:47 -0700

    new data parse

--- dataparse.py
+++ dataparse.py
@@ -1,123 +1,95 @@
 import pandas as pd
+import numpy as np
 import os
-import csv
-import fastparquet
 
-# If we dont already have a dataframe of the parsed ratings data
-if not os.path.isfile('./Data/parseddf.parquet.gzip'):
-    if not os.path.isfile('./Data/data.csv'):
+# Loading the ratings data and filtering out the outliers
+if not os.path.isfile('./Data/removedoutliersdf.parquet.gzip'):
+    # import the rating data as pandas dataframe
+    df = pd.read_csv('./Data/combined_data_1.txt', header=None, names=['UserId', 'Rating'], usecols=[0, 1])
 
-        # read all txt file and store them in one big file
-        data = open('./Data/data.csv', mode='w')
-        row = list()
-        files = ['./Data/combined_data_1.txt', './Data/combined_data_2.txt',
-                 './Data/combined_data_3.txt', './Data/combined_data_4.txt']
-        for file in files:
-            with open(file) as f:
-                for line in f:
-                    del row[:]
-                    line = line.strip()
-                    if line.endswith(':'):
-                        movid_id = line.replace(':', '')
-                    else:
-                        row = [x for x in line.split(',')]
-                        row.insert(0, movid_id)
-                        data.write(','.join(row))
-                        data.write('\n')
-            print('Done.\n')
-        data.close()
+    df['Rating'] = df['Rating'].astype(float)  # Rating is temporarily a float
 
-    df = pd.read_csv('./Data/data.csv', sep=',', names=['MovieId', 'UserId', 'Rating'], usecols=[0, 1, 2])
+    df.index = np.arange(0, len(df))  # reindex the ratings
 
-    # Parsing out the outliers
-    df = df[df.groupby('UserId').transform(len)['Rating'] > 19]  # Remove bottom 10% of UserIds
-    df = df[df.groupby('UserId').transform(len)['Rating'] < 1390]  # Remove top 1% of UserIds
-    df = df[df.groupby('MovieId').transform(len)['Rating'] > 85]  # Remove bottom 10% of MovieIds
+    # Adding the MovieId to the data frame
+    df_nan = pd.DataFrame(pd.isnull(df.Rating))
+    df_nan = df_nan[df_nan['Rating'] == True]
+    df_nan = df_nan.reset_index()
 
-    # Make a parquet file of the dataframe so it is quick to load for later use
-    df.to_parquet('./Data/parseddf.parquet.gzip', compression='gzip')
+    movie_np = []
+    movie_id = 1
 
-    # This is much larger than the parquet file and isnt parsed for outliers. No longer needed.
-    os.remove("./Data/data.csv")
+    for i, j in zip(df_nan['index'][1:], df_nan['index'][:-1]):
+        # numpy approach
+        temp = np.full((1, i-j-1), movie_id)
+        movie_np = np.append(movie_np, temp)
+        movie_id += 1
+
+    last_record = np.full((1, len(df) - df_nan.iloc[-1, 0] - 1), movie_id)
+    movie_np = np.append(movie_np, last_record)
+
+    df = df[pd.notnull(df['Rating'])]
+
+    df['MovieId'] = movie_np.astype(int)
+    df['UserId'] = df['UserId'].astype(int)
 
 
-# The MovieId needs to be reindexed to easily cluster
-if not os.path.isfile('./Data/reindexeddf.parquet.gzip'):
-    # Read the dataframe of the parsed ratings data and write it as a csv.
-    # It is faster to edit the csv than the dataframe.
-    df = pd.read_parquet('./Data/parseddf.parquet.gzip')
-    df.to_csv('./Data/data1.csv', index=False, header=False)
+    # Removing unpopular movies and users with too few reviews
+    # Removing the 70% least popular movies and users with the least ratings
+    f = ['count', 'mean']
 
-    # Making the new indices
-    unique_movieid_list = sorted(df['MovieId'].unique())
-    new_movieid_list = [0] * (unique_movieid_list[-1] + 1) # new_movieid_list["Old"MovieId] = "New"MovieId
-    count = 1
-    for i in unique_movieid_list:
-        new_movieid_list[i] = count
-        count += 1
+    df_movie_summary = df.groupby('MovieId')['Rating'].agg(f)
+    df_movie_summary.index = df_movie_summary.index.map(int)
+    movie_benchmark = round(df_movie_summary['count'].quantile(0.7), 0)
+    drop_movie_list = df_movie_summary[df_movie_summary['count'] < movie_benchmark].index
 
-    # Making new csv to reflect the new movieid's
-    with open('./Data/data1.csv', 'r') as inf, open('./Data/data2.csv', 'w') as outf:
-        reader = csv.reader(inf)
-        writer = csv.writer(outf)
-        for line in reader:
-            line[0] = str(new_movieid_list[int(line[0])])
-            writer.writerow(line)
+    print('Movie minimum times of review: {}'.format(movie_benchmark))
 
-    # Load the new csv and make a parquet file for the new dataframe with new movieids for faster loading.
-    df2 = pd.read_csv('./Data/data2.csv', sep=',', names=['MovieId', 'UserId', 'Rating'], usecols=[0, 1, 2])
-    df2.to_parquet('./Data/reindexeddf.parquet.gzip', compression='gzip')
+    df_cust_summary = df.groupby('UserId')['Rating'].agg(f)
+    df_cust_summary.index = df_cust_summary.index.map(int)
+    cust_benchmark = round(df_cust_summary['count'].quantile(0.7), 0)
+    drop_cust_list = df_cust_summary[df_cust_summary['count'] < cust_benchmark].index
 
-    # Remove the large csv files
-    os.remove('./Data/data1.csv')
-    os.remove('./Data/data2.csv')
+    print('Customer minimum times of review: {}'.format(cust_benchmark))
 
-# Generate the test set. This is 10% of users picked deterministically. The statistical attributes of this set is the
-# same as the training set.
-if not os.path.isfile('./Data/testusersdf.parquet.gzip'):
-    df = pd.read_parquet('./Data/reindexeddf.parquet.gzip')
-    df2 = df.loc[df['UserId'] % 10 == 0]
+    print('Original Shape: {}'.format(df.shape))
+    df = df[~df['MovieId'].isin(drop_movie_list)]
+    df = df[~df['UserId'].isin(drop_cust_list)]
+
+    df['Rating'] = df['Rating'].astype(int)
+
+    print('After Trim Shape: {}'.format(df.shape))
+
     print(df.describe())
-    print(df2.describe())
-    df2.to_parquet('./Data/testusersdf.parquet.gzip', compression='gzip')
+
+    df.to_parquet('./Data/removedoutliersdf.parquet.gzip', compression='gzip')
 
 
-# Generate the training set. This is 90% of users picked deterministically. The statistical attributes of this set is
-# the same as the test set.
+# Get a list of movie titles that passed the above filter
+if not os.path.isfile('./Data/filteredmovietitlesdf.parquet.gzip'):
+    df = pd.read_parquet('./Data/removedoutliersdf.parquet.gzip')
+    df_title = pd.read_csv('./Data/movie_titles.csv', encoding="ISO-8859-1", header=None, usecols=[0, 2],
+                           names=['MovieId', 'Name'])
+    # df_title.set_index('MovieId', inplace=True)
+    df_title = pd.merge(df, df_title, how='inner', on='MovieId').drop_duplicates(subset=['MovieId'])[['MovieId', 'Name']]
+    df_title.to_parquet('./Data/filteredmovietitlesdf.parquet.gzip', compression='gzip')
+
+# Make a training set of users
 if not os.path.isfile('./Data/trainingusersdf.parquet.gzip'):
-    df = pd.read_parquet('./Data/reindexeddf.parquet.gzip')
+    df = pd.read_parquet('./Data/removedoutliersdf.parquet.gzip')
     df2 = df.loc[df['UserId'] % 10 != 0]
+    print("Original data set statistics:")
     print(df.describe())
+    print("Training data set statistics:")
     print(df2.describe())
     df2.to_parquet('./Data/trainingusersdf.parquet.gzip', compression='gzip')
 
-# Generate csv of training data with filename "./Data/traininglist.csv" for the PyClustering library to use
-# The file will have the following format (all values are rating values):
-#
-#       movie1user1 movie2user1 movie3user1 ...  movieMuser1
-#       movie1user2 movie2user2 movie3user2 ...  movieMuser2
-#            ...         ...       ...      ...      ...
-#       movie1userU movie2userU movie3userU ...  movieMuserU
-#
-# Here movie1user1 is the Rating that UserId=1 gave MovieId=1. Here M is the movie with the largest id and U is the
-# user with the largest id. If a user has not rated a movie, then it is 0.
-# if not os.path.isfile('./Data/traininglist.txt'):
-#     df = pd.read_parquet('./Data/trainingusersdf.parquet.gzip')
-#     sortedUserIds = sorted(df['UserId'].unique())  # list of unique UserIds in the training set
-#     maxmovieid = df['MovieId'].max()  # largest movie id
-#     with open('./Data/traininglist.txt', 'w', newline='') as file:
-#         mywriter = csv.writer(file, delimiter=' ')
-#         for uid in sortedUserIds:
-#             # This is a vector representing uid's point in movie preference space.
-#             # We will be appending this to the txt file.
-#             # uidvector = [0, 0, 0, 2, 0, 4, 0, ..., 3] means the user with UserId = uid gave the movie with MovieId = 3 a rating of 2,
-#             # the movie with MovieId = 5 a rating of 4, and the movie with MovieId = maxmovieid a rating of 3.
-#             uidvector = [0] * (maxmovieid + 1)
-#             dfuid = df[df.UserId == uid]
-#             for index, row in dfuid.iterrows():
-#                 uidvector[row['MovieId']] = row['Rating']
-#             mywriter.writerow(uidvector[1:])  # Don't use the first index. There is no movie with MovieId = 0.
-
-df = pd.read_parquet('./Data/trainingusersdf.parquet.gzip')
-df = pd.pivot_table(df, values='Rating', index='UserId', columns='MovieId', fill_value=0)
-print(df.shape)
+# Make a test set of users
+if not os.path.isfile('./Data/testusersdf.parquet.gzip'):
+    df = pd.read_parquet('./Data/removedoutliersdf.parquet.gzip')
+    df2 = df.loc[df['UserId'] % 10 == 0]
+    print("Original data set statistics:")
+    print(df.describe())
+    print("Test data set statistics:")
+    print(df2.describe())
+    df2.to_parquet('./Data/testusersdf.parquet.gzip', compression='gzip')
